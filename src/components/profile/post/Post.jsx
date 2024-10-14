765bdb6687ace27f3498de4e6f7869c0e651d60b
Navneet Singh Solanki <navneetsinghsolanki2580@gmail.com>
2024-10-14 23:34:33 +0530

    profile page done

--- src/components/profile/post/Post.jsx
+++ src/components/profile/post/Post.jsx
@@ -5,16 +5,32 @@ const Post = ({ posts }) => {
     <div className="flex flex-wrap gap-1 mt-1">
       {posts.length > 0 ? (
         posts.map((post) => (
-          <div key={post._id} className="w-[32.2%] md:w-[32.8%] h-32 md:h-40 overflow-hidden flex-shrink-0">
-            <img
-              className="w-full h-full object-cover"
-              src={post.image.url}
-              alt=""
-            />
+          <div
+            key={post._id}
+            className="w-[32.2%] md:w-[32.8%] h-32 md:h-40 overflow-hidden flex-shrink-0"
+          >
+            {post.image.fileType === "image" && (
+              <img
+                className="w-full h-full object-cover"
+                src={post.image.url}
+                alt=""
+              />
+            )}
+            {post.image.fileType === "video" && (
+              <video
+                loop={true}
+                muted={true}
+                autoPlay={true}
+                className="w-full h-full object-cover"
+                src={post.image.url}
+              ></video>
+            )}
           </div>
         ))
       ) : (
-        <p className="w-full text-sm text-center mt-20 text-zinc-400">no post yet.</p>
+        <p className="w-full text-sm text-center mt-20 text-zinc-400">
+          no post yet.
+        </p>
       )}
     </div>
   );
